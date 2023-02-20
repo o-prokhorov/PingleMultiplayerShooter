623e2547987fd83b53a25bbfc4e66f6235341eaf
Oleksandr Prokhorov <o.prokhorov@pinglestudio.com>
2023-02-20 14:49:38 +0200

    setup

--- Source/MultiplayerShooter/MultiplayerShooter.Build.cs
+++ Source/MultiplayerShooter/MultiplayerShooter.Build.cs
@@ -17,7 +17,7 @@ public class MultiplayerShooter : ModuleRules
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
+		//
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
